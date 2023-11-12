2ea922d80ca67d442128fd2e7152d1b4680a9646
Sidharth PT <sidharthanand73@gmail.com>
2023-11-12 10:52:53 +0530

    video call implemented

--- src/infrastructure/config/socketServer.ts
+++ src/infrastructure/config/socketServer.ts
@@ -16,7 +16,34 @@ function initializeSocket(server:any) {
             io.to(room).emit("user:joined", { tutor, id: socket.id });
             socket.join(room);
             io.to(socket.id).emit("room:join", data);
-          });
+        });
+        
+        socket.on('user:call', ({ to, offer }) => {
+            console.log("user:call");
+            
+            io.to(to).emit("incomming:call", {
+                from:socket.id,offer
+              })
+        })
+
+        socket.on("call:accepted", ({ to, ans }) => {
+            io.to(to).emit("call:accepted", {
+                from:socket.id,ans
+              })
+        })
+
+        socket.on("peer:nego:needed", ({ to, offer }) => {
+            io.to(to).emit("peer:nego:needed", {
+                from:socket.id,offer
+              })
+        })
+
+        socket.on("peer:nego:done", ({ to, ans }) => {
+            console.log(`Peer negotiation done from ${socket.id} to ${to}, ans:`, ans);
+            io.to(to).emit("peer:nego:final", {
+                from:socket.id,ans
+              })
+        })
 
     })
 
